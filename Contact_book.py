3f22236d6ffabfa0bcff49abf01a60712645fdb1
Harsh khudania <harshk5603@gmail.com>
2023-08-30 11:30:06 -0700

    third commit

---
+++ Contact_book.py
@@ -0,0 +1,45 @@
+contact = {}
+
+
+def display_contact():
+    print("Name\t\tContact Number")
+    for key in contact:
+        print("{}\t\t{}".format(key,contact.get(key)))
+
+while True:
+    choice = int(input("1. Add New Contact \n 2. Search Contact \n 3. Display Contact \n 4. Edit Contact \n 5. Delete Contact \n 6. Exit \n Enter Your Choice "))
+    if choice == 1:
+        name = input("Enter the Contact Name")
+        phone = input("Enter the Mobile Number")
+        contact[name] = phone
+    elif choice == 2:
+        search_name = input("Enter the Contact Name")
+        if search_name in contact:
+            print(search_name, "'s Contact Number is ", contact[search_name])
+        else:
+            print("Name is not found in Contact Book") 
+    elif choice == 3:
+        if not contact:
+            print("Empty Contact Book")
+        else:
+            display_contact()
+    elif choice == 4:
+        edit_contact = input("Enter the Contact to be edited")
+        if edit_contact in contact:
+            phone = input("Enter Mobile Number")
+            contact[edit_contact]=phone
+            print("Contact updated")
+            display_contact()
+        else:
+            print("Name is not found in Contact Book")  
+    elif choice == 5:
+        del_contact = input("Enter the Contact to be deleted ")
+        if del_contact in contact:
+            confirm = input("Do you want to delete this contact y/n?")
+            if confirm == "y" or confirm == "Y":
+                contact.pop(del_contact)
+            display_contact()
+        else:
+            print("Name is not found in Contact Book")
+    else:
+        break                
